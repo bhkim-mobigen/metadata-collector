e8f0ae427088e993970895215061da174d3ea05e
parkhayoung <parkha816@mobigen.com>
2024-10-08 15:03:32 +0900

    metadata collector 실행 스크립트

---
+++ bin/meta_data_collect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /home/otdev/ot_data_catalog_server/metadata_collector/venv/bin/activate
+
+python3 /home/otdev/ot_data_catalog_server/metadata_collector/src/metadata_ingestion.py $1
+
+deactivate
