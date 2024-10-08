4a296bbde91e1e827920629a73340b6cc6e8fcd5
parkhayoung <parkha816@mobigen.com>
2024-10-08 15:09:41 +0900

    no message

--- bin/meta_data_collect.sh
+++ bin/meta_data_collect.sh
@@ -2,6 +2,6 @@
 
 source /home/otdev/ot_data_catalog_server/metadata_collector/venv/bin/activate
 
-python3 /home/otdev/ot_data_catalog_server/metadata_collector/src/metadata_ingestion.py $1
+python3 /home/otdev/ot_data_catalog_server/metadata_collector/src/metadata_collector.py $1
 
 deactivate
