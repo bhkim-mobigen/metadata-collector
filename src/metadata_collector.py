0c54e83201b0aba5c5fd4248dabbedde5ac075a9
HaYoung P <parkha816@mobigen.com>
2025-01-15 15:58:10 +0900

    메인 수정

--- src/metadata_collector.py
+++ src/metadata_collector.py
@@ -144,13 +144,13 @@ def metadataExecute(system_name, sink="file", sink_host="localhost"):
 if __name__ == "__main__":
     pass
 
-    # metadataExecute(system_name=sys.argv[1],
-    #                 sink='metadata-rest',
-    #                 sink_host='192.168.100.72')
-
-    metadataExecute(system_name='Test-Oracle',
+    metadataExecute(system_name=sys.argv[1],
                     sink='metadata-rest',
-                    sink_host='localhost')
+                    sink_host='192.168.100.72')
+
+    # metadataExecute(system_name='Test-Oracle',
+    #                 sink='metadata-rest',
+    #                 sink_host='localhost')
 
     # metadataExecute(system_name=sys.argv[1], sink="file", sink_host="localhost")
     # metadataExecute(system_name="Test-Oracle", sink="file", sink_host="localhost")
