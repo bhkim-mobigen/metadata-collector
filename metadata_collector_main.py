6dba0e63c2d33c5312350781d16432ea8907c6f5
HaYoung P <parkha816@mobigen.com>
2025-02-11 15:14:03 +0900

    main 수정

--- metadata_collector_main.py
+++ metadata_collector_main.py
@@ -1,18 +1,18 @@
 from src.metadata_collector import metadata_collector_execute
-
+import sys
 
 if __name__ == "__main__":
     pass
 
-    # metadata_collector_execute(system_id=sys.argv[1],
+    metadata_collector_execute(system_id=sys.argv[1],
     # metadata_collector_execute(system_id='6cdfa07e-2253-41fd-b5fb-bb8201b815a3',
-    #                 sink='metadata-rest',
-    #                 sink_host='192.168.100.72')
-
-    metadata_collector_execute(system_id='9f0b3f11-7706-4b07-a319-ac7dd49b2071',
                     sink='metadata-rest',
-                    sink_host='localhost',
-                    sink_port=8585)
+                    sink_host='192.168.100.72')
+
+    # metadata_collector_execute(system_id='9f0b3f11-7706-4b07-a319-ac7dd49b2071',
+    #                 sink='metadata-rest',
+    #                 sink_host='localhost',
+    #                 sink_port=8585)
 
     # metadata_collector_execute(system_name=sys.argv[1], sink="file", sink_host="localhost")
     # metadata_collector_execute(system_id="109ae637-9e13-44f9-9686-a79cf1e12499", sink="file", sink_host="localhost")
