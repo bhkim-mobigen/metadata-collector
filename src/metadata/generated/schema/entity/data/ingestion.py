92fd88eb156f14df3b9c517672bee8393d91231f
HaYoung P <parkha816@mobigen.com>
2025-02-11 15:12:35 +0900

    system info api

--- src/metadata/generated/schema/entity/data/ingestion.py
+++ src/metadata/generated/schema/entity/data/ingestion.py
@@ -16,3 +16,15 @@ class IngestionStatus(BaseModel):
     status: str = Field(..., description='수집 상태')
     err_description: str = Field(None, description='에러 내용')
     user: str = Field(None, description='api 호출자')
+
+
+class MetadataSystemInfo(BaseModel):
+
+    system_id: str = Field(..., description='system_id')
+    system_type: str = Field(..., description='system_type')
+    host: str = Field(..., description='host')
+    port: int = Field(..., description='port')
+    login: str = Field(..., description='login')
+    password: str = Field(..., description='password')
+    database: str = Field(..., description='database')
+    filter_config: str = Field(None, description='filter_config')
