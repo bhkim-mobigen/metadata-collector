84ac5b02e5ccadb0ce760942a8062d12c5f1554a
HaYoung P <parkha816@mobigen.com>
2025-02-12 13:55:05 +0900

    실행 스크립트

--- bin/meta_data_collect.sh
+++ bin/meta_data_collect.sh
@@ -2,7 +2,7 @@
 
 source /home/otdev/ot_data_catalog_server/metadata_collector/venv/bin/activate
 
-export PYTHONPATH=$PYTHONPATH:/home/otdev/ot_data_catalog_server/metadata_collector/src
+export PYTHONPATH=${PYTHONPATH}:/home/otdev/ot_data_catalog_server/metadata_collector/src
 
 python3 /home/otdev/ot_data_catalog_server/metadata_collector/metadata_collector_main.py $1
 
