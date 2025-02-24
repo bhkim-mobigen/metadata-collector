74027038523fa35d3a22f73c2680603987562a44
HaYoung P <parkha816@mobigen.com>
2025-02-24 14:09:58 +0900

    도커 빌드

--- src/utils/process_config.py
+++ src/utils/process_config.py
@@ -33,8 +33,8 @@ class Config(BaseSettings):
 
         metadata_manager_api_config = config_data["metadata_manager.api"]
 
-        sink_host = metadata_manager_api_config[host]
-        sink_port = metadata_manager_api_config[port]
+        sink_host = metadata_manager_api_config["host"]
+        sink_port = metadata_manager_api_config["port"]
 
         metadata_manager_base_url = f"http://{metadata_manager_api_config['host']}:{metadata_manager_api_config['port']}{metadata_manager_api_config['prefix']}"
         get_meta_system_info_api = metadata_manager_api_config['get_meta_system_info_api']
