7c68b1ff8f5275f05a8bd0a8f8728bdcd58ce841
HaYoung P <parkha816@mobigen.com>
2025-04-23 08:40:41 +0900

    altibase 수집 추가

--- metadata_collector_main.py
+++ metadata_collector_main.py
@@ -4,14 +4,14 @@ from metadata_collector import metadata_collector_execute
 if __name__ == "__main__":
     pass
 
-    # metadata_collector_execute(system_id=sys.argv[1],
-    #                 sink='metadata-rest')
+    metadata_collector_execute(system_id=sys.argv[1],
+                    sink='metadata-rest')
 
     # metadata_collector_execute(system_id='38fe4f2a-4030-4770-8dc5-e2fe21009789',
     #                 sink='metadata-rest')
 
     # metadata_collector_execute(system_name=sys.argv[1], sink="file")
-    metadata_collector_execute(system_id="38fe4f2a-4030-4770-8dc5-e2fe21009789", sink="file")
+    # metadata_collector_execute(system_id="38fe4f2a-4030-4770-8dc5-e2fe21009789", sink="file")
     # metadata_collector_execute(system_id="tibero_phy", sink="file")
     # metadata_collector_execute(system_id="altibase_phy", sink="file")
 
