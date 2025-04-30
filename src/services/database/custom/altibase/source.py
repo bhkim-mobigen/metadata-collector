7a7ff20db6506b12e00a5573c9f0b0bd4b81447e
HaYoung P <parkha816@mobigen.com>
2025-04-30 10:29:12 +0900

    odbc 설정에서 dsn은 system_id를 사용하는데 uuid일 경우 길이가 너무 길어서 - 를 제거하고 사용

--- src/services/database/custom/altibase/source.py
+++ src/services/database/custom/altibase/source.py
@@ -1,4 +1,6 @@
 from services.database.database_source import DatabaseSource
+import uuid
+
 class AltibaseSource(DatabaseSource):
 
     def getSourceConfig(self):
@@ -7,10 +9,22 @@ class AltibaseSource(DatabaseSource):
         sourceConfig = SourceConfig(config=DatabaseServiceMetadataPipeline(**self.source_filter))
         return sourceConfig
 
+    def is_valid_uuid(self, id):
+        try:
+            val = uuid.UUID(id)
+            return str(val) == id.lower()
+        except ValueError:
+            return False
+
     def getServiceConnection(self):
         from metadata.generated.schema.entity.services.databaseService import DatabaseConnection
         from metadata.generated.schema.entity.services.connections.database.customDatabaseConnection import \
             CustomDatabaseConnection
+
+        if self.is_valid_uuid(self.system_id):
+            odbcDsnName = self.system_id.replace("-","")
+        else:
+            odbcDsnName = self.system_id
 
         serviceConnection = DatabaseConnection(config=CustomDatabaseConnection(**{
             "type": "CustomDatabase",
@@ -19,7 +33,7 @@ class AltibaseSource(DatabaseSource):
                 "scheme": "altibase+pyodbc",
                 "username": self.source_user,
                 "password": self.source_password,
-                "odbcDsnName": self.system_id.replace("-",""),
+                "odbcDsnName": odbcDsnName,
                 "supportsProfiler": False,
                 "supportsDBTExtraction": True,
                 "supportsMetadataExtraction": True
