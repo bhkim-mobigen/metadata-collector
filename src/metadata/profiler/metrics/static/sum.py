69e31b603214b6a53d825c5934d994923910cec8
hayoung <parkha816@mobigen.com>
2025-09-26 08:19:20 +0900

    feat: 문서 컬럼 프로파일 에러처리

--- src/metadata/profiler/metrics/static/sum.py
+++ src/metadata/profiler/metrics/static/sum.py
@@ -21,6 +21,10 @@ from metadata.profiler.orm.functions.length import LenFn
 from metadata.profiler.orm.functions.sum import SumFn
 from metadata.profiler.orm.registry import is_concatenable, is_quantifiable
 
+from metadata.utils.logger import profiler_logger
+
+logger = profiler_logger()
+
 
 class Sum(StaticMetric):
     """
@@ -49,6 +53,19 @@ class Sum(StaticMetric):
     def df_fn(self, dfs=None):
         """pandas function"""
 
+        print(f"{self.col.name}:{self.col.type}")
+
         if is_quantifiable(self.col.type):
-            return sum(df[self.col.name].sum() for df in dfs)
+            # return sum(df[self.col.name].sum() for df in dfs)
+            return_value = 0
+            try:
+                for df in dfs:
+                    if is_quantifiable(df[self.col.name].dtype):
+                        return None
+                    return_value += df[self.col.name].sum()
+            except Exception as e:
+                logger.error(f"sum value ERROR column name : {self.col.name}, error : {e}")
+                return None
+
+            return return_value
         return None
